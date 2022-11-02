11fcf2a57c7d7dbab068fb751e0d82ad7823158c
MR. Golu <88421428+govaninevil@users.noreply.github.com>
2022-11-02 06:04:24 -0700

    main

--- script/script.js
+++ script/script.js
@@ -1,61 +1,97 @@
 $(document).on('ready', function () {
-  $(document).on('click','.menu-btn',function(event){
-    $('body').toggleClass('menu-open');
+
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 150) {
+      $('header').addClass("small");
+    } else {
+      $('header').removeClass("small");
+    }
   });
 
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 150){
-       $('header').addClass("small");
-    } else {
-       $('header').removeClass("small");
-    }
- });
-
-    $('.regular').slick({
-      dots: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay:true,
-            autoplaySpeed: 2000
-          }
+  $('.regular').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  // collapes script
+  $('.collage.body h3').each(function () {
+    var tis = $(this), state = false, answer = tis.next('div').slideUp();
+    tis.click(function () {
+      state = !state;
+      answer.slideToggle(state);
+      tis.toggleClass('active', state);
+    });
+  });
+
+  $(document).ready(function () {
+    //Horizontal Tab
+    $('#parentHorizontalTab').easyResponsiveTabs({
+      type: 'default', //Types: default, vertical, accordion
+      width: 'auto', //auto or any width like 600px
+      fit: true, // 100% fit in a container
+      tabidentify: 'hor_1', // The tab groups identifier
+      activate: function (event) { // Callback function if tab is switched
+        var $tab = $(this);
+        var $info = $('#nested-tabInfo');
+        var $name = $('span', $info);
+        $name.text($tab.text());
+        $info.show();
+      }
     });
 
-   // collapes script
-   $('.collage.body h3').each(function() {
-    var tis = $(this), state = false, answer = tis.next('div').slideUp();
-    tis.click(function() {
-        state = !state;
-        answer.slideToggle(state);
-        tis.toggleClass('active',state);
+
+
+
+    $("#mobile").on("click", function () {
+      $("body").toggleClass("mobile");
     });
+  });
+
+  $('#mega-menu').easyResponsiveTabs({
+    type: 'default', //Types: default, vertical, accordion
+    width: 'auto', //auto or any width like 600px
+    fit: true, // 100% fit in a container
+    tabidentify: 'hor_2', // The tab groups identifier
+    activate: function(event) { // Callback function if tab is switched
+        var $tab = $(this);
+        var $info = $('#nested-tabInfo');
+        var $name = $('span', $info);
+        $name.text($tab.text());
+        $info.show();
+    }
   });
 });      
