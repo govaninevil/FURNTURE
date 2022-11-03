15402b9a72e1f9ede6b986639b3394fc4aede8c6
MR. Golu <88421428+govaninevil@users.noreply.github.com>
2022-11-03 08:01:50 -0700

    main

--- script/script.js
+++ script/script.js
@@ -1,5 +1,8 @@
 $(document).on('ready', function () {
 
+  $("#mobile").on("click", function () {
+    $("body").toggleClass("mobile");
+  });
 
   $(window).scroll(function () {
     if ($(this).scrollTop() > 150) {
@@ -47,6 +50,33 @@ $(document).on('ready', function () {
     ]
   });
 
+  // client-slider
+  $('.slider-comment').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
   // collapes script
   $('.collage.body h3').each(function () {
     var tis = $(this), state = false, answer = tis.next('div').slideUp();
@@ -57,28 +87,19 @@ $(document).on('ready', function () {
     });
   });
 
-  $(document).ready(function () {
-    //Horizontal Tab
-    $('#parentHorizontalTab').easyResponsiveTabs({
-      type: 'default', //Types: default, vertical, accordion
-      width: 'auto', //auto or any width like 600px
-      fit: true, // 100% fit in a container
-      tabidentify: 'hor_1', // The tab groups identifier
-      activate: function (event) { // Callback function if tab is switched
-        var $tab = $(this);
-        var $info = $('#nested-tabInfo');
-        var $name = $('span', $info);
-        $name.text($tab.text());
-        $info.show();
-      }
-    });
-
-
-
-
-    $("#mobile").on("click", function () {
-      $("body").toggleClass("mobile");
-    });
+  //Horizontal Tab
+  $('#parentHorizontalTab').easyResponsiveTabs({
+    type: 'default', //Types: default, vertical, accordion
+    width: 'auto', //auto or any width like 600px
+    fit: true, // 100% fit in a container
+    tabidentify: 'hor_1', // The tab groups identifier
+    activate: function (event) { // Callback function if tab is switched
+      var $tab = $(this);
+      var $info = $('#nested-tabInfo');
+      var $name = $('span', $info);
+      $name.text($tab.text());
+      $info.show();
+    }
   });
 
   $('#mega-menu').easyResponsiveTabs({
@@ -86,12 +107,12 @@ $(document).on('ready', function () {
     width: 'auto', //auto or any width like 600px
     fit: true, // 100% fit in a container
     tabidentify: 'hor_2', // The tab groups identifier
-    activate: function(event) { // Callback function if tab is switched
-        var $tab = $(this);
-        var $info = $('#nested-tabInfo');
-        var $name = $('span', $info);
-        $name.text($tab.text());
-        $info.show();
+    activate: function (event) { // Callback function if tab is switched
+      var $tab = $(this);
+      var $info = $('#nested-tabInfo');
+      var $name = $('span', $info);
+      $name.text($tab.text());
+      $info.show();
     }
   });
 });      
